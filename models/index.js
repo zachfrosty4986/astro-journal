bba1c37ae740a86d82c99ccf4910a84d742c49bc
Camarco <camarcovincent@gmail.com>
2024-06-06 20:02:01 -0400

    added models, updated git ignore, added server.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,15 @@
 const User = require('./User')
 const Blog = require('./Blog')
 
-module.exports = { User, Blog}
+//relationship stating a user can have many blogs
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+//relationship stating a blog belongs to a user
+Blog.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+module.exports = { User, Blog }
