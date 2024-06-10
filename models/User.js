98d97fb48a4e3d6ccda6a0a3cb36b01d6a5849a0
zachfrosty4986 <zachjames0114@gmail.com>
2024-06-10 19:22:56 -0400

    Debugged file path for api routes to connection.js

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
-const sequelize = require('../server/config/connection')
+const sequelize = require('../server/config/connection.js')
 
 class User extends Model {
     checkPassword(loginPw) {
