0acf99e1f970d7ef2627a2ce9267d14b40081058
Camarco <camarcovincent@gmail.com>
2024-06-13 20:07:34 -0400

    added profile button, changed login from asking for username to password

--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,7 @@
 const login = async (event) => {
     event.preventDefault();
 
-    const email = document.querySelector('#username').value
+    const email = document.querySelector('#email').value
     const password = document.querySelector('#password').value
 
     if (email && password) {
